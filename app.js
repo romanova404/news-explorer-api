6e819d0827aab4dbe262eef9bf943dd5ca7ed10b
Iuliia Romanova <romanova.works@yandex.ru>
2020-08-02 17:44:43 +0200

    js test attempt 9

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ mongoose.connect(NODE_ENV === 'production' ? MONGO_URL : mongoUrlDev, {
   useUnifiedTopology: true,
 });
 
-const corsList = ['https://romanova404.github.io/', 'http://localhost:8080', 'https://yandexstudyproject.design'];
+const corsList = ['https://romanova404.github.io/', 'http://localhost:8080', 'https://yandexstudyproject.design', 'https://www.yandexstudyproject.design'];
 const corsOptions = {
   origin: corsList,
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
